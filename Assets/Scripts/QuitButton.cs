b9cc27edf4d42ac173ec94ed4c9b4b714ea0e8fe
Aliev Rasul <arasul110294@gmail.com>
2021-02-06 22:17:33 +0300

    added quit button script;

---
+++ Assets/Scripts/QuitButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class QuitButton : MonoBehaviour
+{
+    private Button _quitButton;
+    
+    private void Awake()
+    {
+        _quitButton = GetComponent<Button>();
+
+        if (_quitButton == null)
+            throw new Exception("No Button component");
+        
+        _quitButton.onClick.AddListener(QuitGame);
+    }
+
+    private void OnDestroy()
+    {
+        _quitButton.onClick.RemoveListener(QuitGame);
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
+}
