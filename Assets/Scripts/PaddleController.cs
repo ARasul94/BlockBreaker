576ca890fac3a1a1e02f00146f5337d02d90e8ed
Aliev Rasul <arasul110294@gmail.com>
2021-02-07 02:03:34 +0300

    added paddle controller;

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    [SerializeField] private float screenWidthInUnits = 16;
+    [SerializeField] private float minX = -7;
+    [SerializeField] private float maxX = 7;
+    
+
+    private void Update()
+    {
+        float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits - screenWidthInUnits / 2;
+        mousePositionInUnits = Mathf.Clamp(mousePositionInUnits, minX, maxX);
+        
+        Vector2 paddlePosition = new Vector2(mousePositionInUnits, transform.position.y);
+        transform.position = paddlePosition;
+    }
+}
