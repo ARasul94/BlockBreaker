1b2b3351c21cba784d7aecc628b53f4d0bec5f7b
Aliev Rasul <arasul110294@gmail.com>
2021-02-06 22:07:35 +0300

    added SceneLoader script;

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private int _curSceneIndex;
+    private int _sceneCount;
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        _curSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        _sceneCount = SceneManager.sceneCountInBuildSettings;
+    }
+
+    public void LoadNextScene()
+    {
+        if (_curSceneIndex < _sceneCount - 1)
+        {
+            _curSceneIndex++;
+        }
+        else
+        {
+            _curSceneIndex = 0;
+        }
+        SceneManager.LoadScene(_curSceneIndex);
+    }
+}
