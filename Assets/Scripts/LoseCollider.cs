bce257cd057ccdfb426e1f8c265122bb234403cc
Aliev Rasul <arasul110294@gmail.com>
2021-02-07 01:43:43 +0300

    added LoseCollider.cs script;

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class LoseCollider : MonoBehaviour
+{
+    private SceneLoader _sceneLoader;
+
+    private void Awake()
+    {
+        _sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        _sceneLoader.LoadGameOverScene();
+    }
+}
