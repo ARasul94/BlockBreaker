e0dd48906c7ca1fec983f19bb4e6d33d0bec0f41
Aliev Rasul <arasul110294@gmail.com>
2021-02-07 22:53:20 +0300

    update player score from game status controller;

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -7,8 +7,19 @@ public class GameStatus : MonoBehaviour
 {
     [Range(0, 3)][SerializeField] private float gameSpeed = 1;
     
-    private int _score = 0;
-    
+    private int _score;
+    private ScoreController _scoreController;
+
+    private void Awake()
+    {
+        _scoreController = FindObjectOfType<ScoreController>();
+    }
+
+    private void Start()
+    {
+        _score = 0;
+    }
+
     private void Update()
     {
         Time.timeScale = gameSpeed;
@@ -17,5 +28,6 @@ public class GameStatus : MonoBehaviour
     public void AddScore(int additionalScore)
     {
         _score += additionalScore;
+        _scoreController.UpdateDisplayedScore(_score);
     }
 }
