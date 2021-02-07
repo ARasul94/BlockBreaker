5d97f8647f07b2edc7145702ffd9cd52f7dd9a95
Aliev Rasul <arasul110294@gmail.com>
2021-02-07 22:20:52 +0300

    added level control methods to LevelController.cs

--- Assets/Scripts/BlockController.cs
+++ Assets/Scripts/BlockController.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BlockController : MonoBehaviour
 {
     [SerializeField] private AudioClip breakSound;
-    
+
+    private LevelController _levelController;
+
+    private void Awake()
+    {
+        _levelController = FindObjectOfType<LevelController>();
+    }
+
+    private void Start()
+    {
+        _levelController.AddBreakableBlock();
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
+        _levelController.RemoveBreakableBlock();
     }
 }
--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -1,8 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
+    private int _breakableBlocksCount = 0;
+    private SceneLoader _sceneLoader;
+
+    private void Awake()
+    {
+        _sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void AddBreakableBlock()
+    {
+        _breakableBlocksCount++;
+    }
     
+    public void RemoveBreakableBlock()
+    {
+        _breakableBlocksCount--;
+
+        if (_breakableBlocksCount <= 0)
+        {
+            _sceneLoader.LoadNextScene();
+        }
+    }
 }
