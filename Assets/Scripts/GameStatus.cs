e11101a93ba1b2e107fa9c208954de8b8b5f5903
Aliev Rasul <arasul110294@gmail.com>
2021-02-07 22:32:02 +0300

    added  gamestatus with game speed controller

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    [Range(0, 3)][SerializeField] private float gameSpeed = 1;
+
+    private void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+}
