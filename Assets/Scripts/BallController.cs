46d3e03839f0051eee52b52a58c805b5cc9dac5e
Aliev Rasul <arasul110294@gmail.com>
2021-02-07 15:54:03 +0300

    added logic to connect ball position with paddle

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PaddleController _paddle;
+    private Vector3 _offset;
+    private void Awake()
     {
-        
+        _paddle = FindObjectOfType<PaddleController>();
+        _offset = new Vector3(0, _paddle.BallOffset, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        UpdateBallPositionOnPaddle();
+    }
+
+    private void LateUpdate()
+    {
+        UpdateBallPositionOnPaddle();
+    }
+
+    private void UpdateBallPositionOnPaddle()
+    {
+        transform.position = _paddle.transform.position + _offset;
     }
 }
