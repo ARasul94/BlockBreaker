647a3fe1878c34c31af881d64cf6d93e7734f958
Aliev Rasul <arasul110294@gmail.com>
2021-02-07 22:50:47 +0300

    added score controller to canvas;

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreTextfield;
+
+    private void Start()
+    {
+        scoreTextfield.text = string.Empty;
+    }
+
+    public void UpdateDisplayedScore(int score)
+    {
+        scoreTextfield.text = score.ToString();
+    }
+}
