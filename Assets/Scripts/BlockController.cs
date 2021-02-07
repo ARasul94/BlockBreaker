061ae1a56b9b39539a478870cd227ad62c91e3f7
Aliev Rasul <arasul110294@gmail.com>
2021-02-07 23:25:44 +0300

    added sparkle vfx;

--- Assets/Scripts/BlockController.cs
+++ Assets/Scripts/BlockController.cs
@@ -5,6 +5,8 @@ public class BlockController : MonoBehaviour
 {
     [SerializeField] private AudioClip breakSound;
     [SerializeField] private int scoreForDestroying = 10;
+    [SerializeField] private GameObject sparkleVFX;
+    
 
     private LevelController _levelController;
     private GameStatus _gameStatus;
@@ -27,9 +29,26 @@ public class BlockController : MonoBehaviour
 
     private void DestroyBlock()
     {
-        _gameStatus.AddScore(scoreForDestroying);
-        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        PlaySFX();
+        PlayVFX();
+        AddScore();
         Destroy(gameObject);
         _levelController.RemoveBreakableBlock();
+    }
+
+    private void PlaySFX()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+    }
+
+    private void PlayVFX()
+    {
+        var sparkles = Instantiate(sparkleVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 1);
+    }
+
+    private void AddScore()
+    {
+        _gameStatus.AddScore(scoreForDestroying);
     }
 }
