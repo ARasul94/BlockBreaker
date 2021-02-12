43661a1e39300cd8bfb2d4d5601bcf67ac5ffde0
Arkhipov Roman <ya.pfccska@yandex.ru>
2021-02-12 21:06:51 +0300

    Click scene

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoseCollider : MonoBehaviour {
-    
+public class LoseCollider : MonoBehaviour
+{
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene("Game Over");
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
+
 }
