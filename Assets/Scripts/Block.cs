0d4aec54c98c8fa48d18b739e2a3a0ba38a8f5ca
Arkhipov Roman <ya.pfccska@yandex.ru>
2021-02-14 17:53:11 +0300

    24 lession

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,9 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Block : MonoBehaviour 
+public class Block : MonoBehaviour
 {
+    private Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private int breakableBlocks;
+
+    private SceneLoader sceneloader;
+
+    private void Start()
+    {
+        sceneloader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneloader.LoadNextScene();
+        }    
+    }
+}
